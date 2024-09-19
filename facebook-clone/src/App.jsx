cbeb1dc1d2c8e7d46dfed23d5a57f0c8fd3ee145
adiflp <adifulop23@gmail.com>
2024-09-19 13:58:56 +0200

    Login v2.0

--- facebook-clone/src/App.jsx
+++ facebook-clone/src/App.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import './App.css'
-import LoginPage from './LoginPage/LoginPage'
+import React from "react";
+import "./App.css";
+import LoginPage from "./LoginPage/LoginPage";
+import AddIcon from "@mui/icons-material/Add";
 
 function App() {
-
   return (
     <>
-      <LoginPage/>
+      <LoginPage />
+      <div className="languages">
+        <a href="Espanol">Español (España)</a>
+        <a href="Catala">Catalá</a>
+        <a href="Frances">Français (France)</a>
+        <a href="Rumano">Română</a>
+        <a href="Italiano">Italiano</a>
+        <a href="Gakegi">Galego</a>
+        <a href="Aleman">Deutsch</a>
+        <a href="Portuges">Português (Brasil)</a>
+        <a href="Arabe"> العربية हिन्दी</a>
+        <button className="btnPlus">
+          <AddIcon fontSize="extrasmall" />
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
